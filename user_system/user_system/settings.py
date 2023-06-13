a2ce4290be6f36b8f66bfe8b488e4697e58cd694
ronnyerrmann <36272471+ronnyerrmann@users.noreply.github.com>
2023-06-13 09:57:42 +0100

    Allow local host connection for non-debug - runs

--- user_system/user_system/settings.py
+++ user_system/user_system/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fd^lp=#1u9ww&8-fen#!-*dawcjn(-sh1wch#30s5q9427jui7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
